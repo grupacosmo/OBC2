88de588a1deb8e7449d7186898476cd68f0fd936
Maciej Rybiński <58656716+rvbnsk@users.noreply.github.com>
2022-12-14 06:55:04 +0100

    Added Configuration (#1)

---
+++ include/source_location.hpp
@@ -0,0 +1,53 @@
+/// The SourceLocation class represents the location in the source code.
+/// It includes filename, function name and line number.
+///
+/// SourceLocation can be used as a function's default parameter to
+/// automatically get the location of the function call.
+///
+/// # Examples
+///
+/// ```
+/// void log(SourceLocation loc = SourceLocation::current())
+/// {
+///     std::printf(%s:%s:%d, loc.file(), loc.function(), loc.line());
+/// }
+///
+/// int main()
+/// {
+///     log(); // prints main.cpp:main:8
+/// }
+/// ```
+
+#ifndef OBC_SOURCE_LOCATION_HPP
+#define OBC_SOURCE_LOCATION_HPP
+
+namespace obc {
+
+class SourceLocation {
+    const char* file_ = "unknown";
+    const char* function_ = file_;
+    int line_ = 0;
+
+   public:
+    constexpr SourceLocation() = default;
+
+    static constexpr SourceLocation current(
+        const char* file = __builtin_FILE(),
+        const char* function = __builtin_FUNCTION(),
+        int line = __builtin_LINE())
+    {
+        SourceLocation loc;
+        loc.file_ = file;
+        loc.function_ = function;
+        loc.line_ = line;
+        return loc;
+    }
+
+    constexpr const char* file() const { return file_; }
+    constexpr const char* function() const { return function_; }
+    constexpr int line() const { return line_; }
+};
+
+}  // namespace obc
+
+#endif
---
+++ script/framework_unflags.py
@@ -0,0 +1,11 @@
+# blackpill_f411ce packages uses the register keyword
+# this script suppreses the warning in c++17 build
+# where register is deprecated keyword
+
+Import("env")
+
+framework_unflags = [ "-Wno-register" ]
+
+env.Append(
+    CXXFLAGS = framework_unflags
+)
