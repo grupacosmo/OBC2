d61e3fbf0d985194681262871a4eff4b06627a00
rvbnsk <58656716+rvbnsk@users.noreply.github.com>
2023-01-18 20:35:10 +0100

    added barometer functionality

---
+++ include/barometer.hpp
@@ -0,0 +1,23 @@
+#ifndef OBC_BAROMETER_HPP
+#define OBC_BAROMETER_HPP
+
+#include <BMP280.h>
+
+#include "error.hpp"
+#include "result.hpp"
+
+namespace obc {
+
+struct BmpMeasurements {
+    double temperature;
+    double pressure;
+    double altitude;
+};
+
+Result<Unit, Errc> init(BMP280& bmp);
+Result<BmpMeasurements, Errc> measure(BMP280& bmp);
+void print(BmpMeasurements measurements);
+
+}  // namespace obc
+
+#endif
