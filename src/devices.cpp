c8b6ceee1a239dc0e394ee86265ccdb483072424
rvbnsk <58656716+rvbnsk@users.noreply.github.com>
2023-01-29 19:42:55 +0100

    added devices setup

---
+++ src/devices.cpp
@@ -0,0 +1,43 @@
+#include "devices.hpp"
+
+#include <Arduino.h>
+
+extern MMA8452Q accelerometer;
+extern BMP280 bmp;
+extern Adafruit_GPS gps;
+
+namespace obc {
+
+void init()
+{
+    pinMode(D6, OUTPUT);
+    sd_init().expect("SD init failure");
+
+    if (auto result = init(accelerometer); result.is_err()) {
+        log_error_and_panic(
+            String("Accelerometer not initialized properly, errc: ")
+            + utl::to_underlying(result.unwrap_err()));
+    }
+
+    if (auto result = init(bmp); result.is_err()) {
+        log_error_and_panic(
+            String("Barometer not initialized properly, errc: ")
+            + utl::to_underlying(result.unwrap_err()));
+    }
+
+    if (auto result = init(gps); result.is_err()) {
+        log_error_and_panic(
+            String("GPS not initialized properly, errc: ")
+            + utl::to_underlying(result.unwrap_err()));
+    }
+
+    if (auto result = init_lora(); result.is_err()) {
+        log_error_and_panic(
+            String("Lora not initialized properly, errc: ")
+            + utl::to_underlying(result.unwrap_err()));
+    }
+
+    log_boot("Devices initialized properly.");
+}
+
+}  // namespace obc
